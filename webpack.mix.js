a911157f2ef8a0212b9365d4f5b54ebcecad7519
Marzia Fatarella <marziafatarella@gmail.com>
2020-06-08 17:39:39 +0200

    pronti

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'public/').sass('src/app.scss', 'public/');
+mix.js('src/js/app.js', 'public/').sass('src/scss/app.scss', 'public/');
 
 // Full API
 // mix.js(src, output);
